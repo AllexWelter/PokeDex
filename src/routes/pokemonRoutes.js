9bfb2667d33e93f6d8c4cc06195f5fc29afd7cfb
Allex Welter <allexwelter13@gmail.com>
2024-12-06 09:39:36 -0300

    Update pokemonRoutes.js

--- src/routes/pokemonRoutes.js
+++ src/routes/pokemonRoutes.js
@@ -1,5 +1,5 @@
 import { Router} from 'express'
-import { getPokemon } from '../controllers/pokemonControllers'
+import { getPokemon } from '../controllers/pokemonControllers.js'
 
 const router = Router()
 
