a999b3b93d402cd892b6af553ec864bc8f6730cb
Allex Welter <allexwelter13@gmail.com>
2024-12-06 08:42:35 -0300

    Update db.js

---
+++ src/database/db.js
@@ -0,0 +1,18 @@
+import mysql from "mysql2"
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,   
+    user: 'root',
+    password: 'sua_senha',
+    database: 'pokedex',
+});
+
+connection.then(() => {
+    console.log('Conectado ao banco de dados')
+}).catch((error) =>{
+    console.error(error.message)
+    throw new Error('Erro ao conectar ao banco de dados')
+})
+
+export default connection
