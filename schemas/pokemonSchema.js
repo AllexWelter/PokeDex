a3d400efe225acb11975078f360626d469ecab84
Allex Welter <allexwelter13@gmail.com>
2024-12-17 07:01:32 -0300

    Update pokemonSchema.js

--- schemas/pokemonSchema.js
+++ schemas/pokemonSchema.js
@@ -1,5 +1,9 @@
 import * as yup from 'yup'
 
 const pokemonSchema = yup.object().shape({
-    
+    id: yup.string().positive().integer().required(),
+    nome: yup.string().required(),
+    tipo: yup.string().required()
 })
+
+export default pokemonSchema
