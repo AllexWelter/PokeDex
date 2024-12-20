b8a0e5668ed0dd0adb47285a547d737f67573a51
Allex Welter <allexwelter13@gmail.com>
2024-12-20 07:42:30 -0300

    Update App.jsx

--- pokedex-frontend/src/App.jsx
+++ pokedex-frontend/src/App.jsx
@@ -4,4 +4,19 @@ import axios from 'axios'
 function App() {
   const [pokemons, setPokemon] = useState([])
   const [loading, setLoading] = useState(true)
+  const [erros, setError] = useState(null)
+
+  useEffect(() => {
+    const fetchPokemons = async () => {
+      try {
+        setLoading(true)
+        const response = await axios.get('http://localhost:3000/api/pokemons')
+        setPokemons(response.data)
+      } catch (error) {
+        setError(error)
+      } finally {
+        setLoading(false)
+      }  
+    }
+  })
 }
