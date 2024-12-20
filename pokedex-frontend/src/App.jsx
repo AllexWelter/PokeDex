e7d60cfe395a2598a007f4686562214763e4e524
Allex Welter <allexwelter13@gmail.com>
2024-12-20 07:35:13 -0300

    Update App.jsx

--- pokedex-frontend/src/App.jsx
+++ pokedex-frontend/src/App.jsx
@@ -1,2 +1,7 @@
 import React, { useState, useEffect} from React
 import axios from 'axios'
+
+function App() {
+  const [pokemons, setPokemon] = useState([])
+  const [loading, setLoading] = useState(true)
+}
