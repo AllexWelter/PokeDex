6fc8a85a7d8ecc7512307e09bf300161fe43dd5c
Allex Welter <allexwelter13@gmail.com>
2024-12-22 06:14:54 -0300

    Update PokemonList.jsx

--- pokedex-frontend/src/components/PokemonList.jsx
+++ pokedex-frontend/src/components/PokemonList.jsx
@@ -2,11 +2,19 @@ import React, {useState, useEffect} from 'react'
 import axios from 'axios'
 
 function PokemonList({ onFilter }) {
-    const [types, setTypes] = [useState([])]
+    const [types, setTypes] = useState([])
 
     useEffect(() => {
-        
-    })
+        const fetchTypes = async () => {
+            try {
+                const response = await axios.get('https://pokeapi.co/api/v2/type')
+                setTypes(response.data.results)
+            } catch (error) {
+                console.error('Erro ao buscar tipos de Pokémons:', error)
+            }
+        }
+        fetchTypes()            
+    }, [])  // Array vazio como segundo argumento garante que o useEffect seja executado apenas uma vez
 
 } 
 
