6190357a0d59513b910afe9851b75008ff14a319
Allex Welter <allexwelter13@gmail.com>
2024-12-22 08:14:20 -0300

    Update PokemonCard.jsx

--- pokedex-frontend/src/components/PokemonCard.jsx
+++ pokedex-frontend/src/components/PokemonCard.jsx
@@ -1,14 +1,18 @@
 import React from 'react'
 
 function PokemonCard({ pokemon }) {
+    if (!pokemon || !pokemon.sprites) {
+        return <div>Carregando...</div>
+    }
+
     return (
         <div className='pokemon-card'>
             <img
                 src={pokemon.sprites.front_default}
                 alt={pokemon.name}
             />
-            <h3>{pokemon.name}</h3>
-            <p>Tipo: {pokemon.types.map(type => type.type.name).join(', ')}</p>
+            <h3>{pokemon.name}</h3> {/* Nome do Pokémon */}
+            <p>Tipo: {pokemon.types.map(type => type.type.name).join(', ')}</p> {/* Tipo(s) do Pokémon */}
         </div>
     )
 }
