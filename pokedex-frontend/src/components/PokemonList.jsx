6c748d5fc3b4f80e159336cb561e94cf57240523
Allex Welter <allexwelter13@gmail.com>
2024-12-22 06:50:33 -0300

    Update PokemonList.jsx

--- pokedex-frontend/src/components/PokemonList.jsx
+++ pokedex-frontend/src/components/PokemonList.jsx
@@ -1,2 +1,16 @@
 import React from 'react'
 import PokemonCard from './PokemonCard'
+
+function PokemonList({ pokemons }) {
+    return (
+        <ul className="pokemon-list">
+        {pokemons.map(pokemon => (
+        <li key={pokemon.id}>
+            <PokemonCard pokemon={pokemon} />
+        </li>    
+        ))}
+        </ul>
+    )
+}
+
+export default PokemonList
