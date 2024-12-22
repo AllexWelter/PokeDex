eaaca93dd5793a5c27f412ec028b312d2d4077b2
Allex Welter <allexwelter13@gmail.com>
2024-12-22 05:44:02 -0300

    Update PokemonList.js

--- pokedex-frontend/src/components/PokemonList.js
+++ pokedex-frontend/src/components/PokemonList.js
@@ -1,1 +1,11 @@
 import React from 'react'
+
+function PokemonList() {
+    return (
+        <ul>
+            {/*Aqui vc ira iterar sobre a lista de pokemons e exibir cada um */}
+        </ul>
+    )
+}
+
+export default PokemonList
