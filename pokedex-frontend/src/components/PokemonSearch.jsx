af6dec0d112042cc5e26428a583895c4d2fa79fe
Allex Welter <allexwelter13@gmail.com>
2024-12-22 07:05:56 -0300

    Update PokemonSearch.jsx

--- pokedex-frontend/src/components/PokemonSearch.jsx
+++ pokedex-frontend/src/components/PokemonSearch.jsx
@@ -1,1 +1,28 @@
 import React, { useState } from 'react'
+
+function PokemonSearch({ onSearch }) {
+    const [searchTerm, setSearchTerm] = useState('')
+
+    const handleChange = (event) => {
+        setSearchTerm(event.target.value)
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        onSearch(searchTerm)
+    }
+
+    return(
+        <form onSubmit={handleSubmit}>
+            <input
+            type='text'
+            placeholder='Busca Pokémon...'
+            value={searchTerm}
+            onChange={handleChange}
+            />
+            <button type='submit'>Buscar</button>
+        </form>
+    )
+}
+
+export default PokemonSearch
