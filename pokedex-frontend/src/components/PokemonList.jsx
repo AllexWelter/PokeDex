970cb9263bb7c110531759700c4ccc90c4b28888
Allex Welter <allexwelter13@gmail.com>
2024-12-22 06:10:24 -0300

    Update PokemonList.jsx

--- pokedex-frontend/src/components/PokemonList.jsx
+++ pokedex-frontend/src/components/PokemonList.jsx
@@ -1,12 +1,14 @@
 import React, {useState, useEffect} from 'react'
 import axios from 'axios'
 
-function PokemonList() {
-    return (
-        <ul>
-            {/*Aqui vc ira iterar sobre a lista de pokemons e exibir cada um */}
-        </ul>
-    )
-}
+function PokemonList({ onFilter }) {
+    const [types, setTypes] = [useState([])]
+
+    useEffect(() => {
+        
+    })
+
+} 
+
 
 export default PokemonList
