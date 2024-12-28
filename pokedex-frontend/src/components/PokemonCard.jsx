e6ed52c28d69e33207e1d6070d407cf5bc60e5e0
Allex Welter <allexwelter13@gmail.com>
2024-12-28 08:34:01 -0300

    Update PokemonCard.jsx

--- pokedex-frontend/src/components/PokemonCard.jsx
+++ pokedex-frontend/src/components/PokemonCard.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 function PokemonCard({ pokemon }) {
-    console.log('Dados do Pokemon no PokemonCard', pokemon)
+    console.log('Sprites do Pokemon', pokemon.sprites)
     if (!pokemon || !pokemon.sprites || Object.keys(pokemon.sprites).length === 0) {
         return <div>Carregando...</div>
     }
