cb8e939ce7605a6c62894b3a19a2432164a8c368
Allex Welter <allexwelter13@gmail.com>
2024-12-28 07:14:57 -0300

    Update App.jsx

--- pokedex-frontend/src/App.jsx
+++ pokedex-frontend/src/App.jsx
@@ -10,6 +10,7 @@ function App() {
   const [error, setError] = useState(null)
   const [searchTerm, setSearchTerm] = useState('')
   const [selectedType, setSelectedType] = useState('')
+  const [listKey, setListkey] = useState(0)
 
   useEffect(() => {
     const fetchPokemons = async () => {
